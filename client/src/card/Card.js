0d3a126c433d6bbd0f2f4d9b522770a1be87e72d
hatim_nourbay <hatim_nourbay@bitbucket.org>
2019-04-25 18:30:48 +0100

    continue card game

--- client/src/card/Card.js
+++ client/src/card/Card.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import shuffle from 'lodash/shuffle'
+import HallOfFame, { FAKE_HOF } from './HallOfFame'
 
 import './Card.css'
 
 const HIDDEN_SYMBOL = '❓'
+const SIDE = 6
+const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
 
 const Card = ({ card, feedback, onClick }) => (
     <div className={`card ${feedback}`} onClick={() => onClick(card, feedback)}>
@@ -12,21 +16,33 @@ const Card = ({ card, feedback, onClick }) => (
     </div>
   )
 
-export class CardGame extends React.Component {
-    
-handleCardClick(card, feedback) {
-    console.log(feedback, 'clicked')
+var cards = ({}) => {
+    const result = []
+    const size = SIDE * SIDE
+    const candidates = shuffle(SYMBOLS)
+    while (result.length < size) {
+      const card = candidates.pop()
+      result.push(card, card)
+    }
+    return shuffle(result)
   }
 
+function handleCardClick(card, feedback) {
+    console.log(feedback, 'clicked')
+  }
+const won = true;
+
+export class CardGame extends React.Component {
     render() {
         return (
             <div className="memory">
-        <Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
-        <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
-        <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick} />
-        <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
-        <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
-        <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
+        <Card card="😀" feedback="hidden" onClick={handleCardClick} />
+        <Card card="🎉" feedback="justMatched" onClick={handleCardClick} />
+        <Card card="💖" feedback="justMismatched" onClick={handleCardClick} />
+        <Card card="🎩" feedback="visible" onClick={handleCardClick} />
+        <Card card="🐶" feedback="hidden" onClick={handleCardClick} />
+        <Card card="🐱" feedback="justMatched" onClick={handleCardClick} />
+        {won && <HallOfFame entries={FAKE_HOF} />}
       </div>
         )
       }
